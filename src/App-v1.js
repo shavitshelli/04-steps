9aa0b596d7ce441db4c2fd8505ac1334391a0190
Shavit Shelli <shavit.shelli57@gmail.com>
2023-10-16 20:10:10 +0300

    added use of children prop

--- src/App-v1.js
+++ src/App-v1.js
@@ -33,40 +33,25 @@ export default function App() {
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
 
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
           <div className="buttons">
-            <Button
-              textColor={"white"}
-              bgColor={"#7950f2"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
               onClick={handlePrev}
             >
-              <span>👈</span> Previuos
-            </Button>
-
-            <Button textColor="white" bgColor="#7950f2" onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+              Previuos
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-}
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3> {children}
-    </div>
   );
 }
